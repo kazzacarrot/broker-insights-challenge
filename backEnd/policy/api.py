a2544ba26672baf06cda23e8aa122ac9cc3327d3
Karen McCulloch <k.a.z.mcc95@gmail.com>
2018-05-30 01:01:48 +0100

    Enable filtering on the api

--- backEnd/policy/api.py
+++ backEnd/policy/api.py
@@ -1,19 +1,29 @@
 from policy.models import Customer, Insurer, Policy, PolicyType
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from tastypie.fields import ForeignKey, CharField
-# from django.db.models import CharField, F
 
 
 class InsurerResource(ModelResource):
     class Meta:
         queryset = Insurer.objects.all()
         include_resource_uri = False
+        filtering = {
+                "id": ALL,
+                "name": ALL,
+        }
 
 
 class CustomerResource(ModelResource):
     class Meta:
         queryset = Customer.objects.all()
         include_resource_uri = False
+
+        filtering = {
+                "id": ALL,
+                "name": ALL,
+                "address": ALL,
+        }
+
 
 class PolicyResource(ModelResource):
     insurer = ForeignKey(InsurerResource, "insurer", full=True)
@@ -24,4 +34,11 @@ class PolicyResource(ModelResource):
         queryset = Policy.objects.all().annotate()
         include_resource_uri = False
 
+        filtering = {
+                "id": ALL,
+                "premium": ALL,
+                "customer": ALL_WITH_RELATIONS,
+                "insurer": ALL_WITH_RELATIONS,
+                "policy_type": ALL_WITH_RELATIONS,
+        }
     
